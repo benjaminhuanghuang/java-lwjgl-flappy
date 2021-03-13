727b40776812874869fb9d55df2b06d989b535e9
benjaminhuanghuang <bhuang@rms.com>
2021-03-12 16:31:33 -0800

    add codee

--- flappy/src/main/java/ben/study/codesnippets/MainSkeleton.java
+++ flappy/src/main/java/ben/study/codesnippets/MainSkeleton.java
@@ -1,23 +1,18 @@
-package ben.study.flappy;
+
+
+package ben.study.codesnippets;
+
+import ben.study.flappy.Main;
+import org.lwjgl.glfw.GLFWVidMode;
 
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
-import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL13.*;
-import static org.lwjgl.system.MemoryUtil.*;
 
-import java.nio.ByteBuffer;
 
-import org.lwjgl.glfw.GLFWVidMode;
-
-import ben.study.flappy.graphics.Shader;
-import ben.study.flappy.Input;
-import ben.study.flappy.layer.Level;
-import ben.study.flappy.maths.Matrix4f;
-
-public class Main implements Runnable {
+public class MainSkeleton implements Runnable {
     private int width = 1280;
     private int height = 720;
 
@@ -26,7 +21,6 @@ public class Main implements Runnable {
 
     private long window;
 
-    private Level level;
 
     public void start() {
         running = true;
@@ -55,8 +49,6 @@ public class Main implements Runnable {
                 (vidmode.height() - height) / 2
         );
 
-        glfwSetKeyCallback(window, new Input());
-
         //
         glfwMakeContextCurrent(window);
         glfwShowWindow(window);
@@ -67,47 +59,13 @@ public class Main implements Runnable {
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         System.out.println("OpenGL: " + glGetString(GL_VERSION));
 
-        Shader.loadAll();
-
-        Matrix4f pr_matrix = Matrix4f.orthographic(-10.0f, 10.0f, -10.0f * 9.0f / 16.0f, 10.0f * 9.0f / 16.0f, -1.0f, 1.0f);
-        Shader.BG.setUniformMat4f("pr_matrix", pr_matrix);
-        Shader.BG.setUniform1i("tex", 1);
-
-        Shader.BIRD.setUniformMat4f("pr_matrix", pr_matrix);
-        Shader.BIRD.setUniform1i("tex", 1);
-
-        Shader.PIPE.setUniformMat4f("pr_matrix", pr_matrix);
-        Shader.PIPE.setUniform1i("tex", 1);
-
-        level = new Level();
     }
 
     public void run() {
         init();
-
-        long lastTime = System.nanoTime();
-        double delta = 0.0;
-        double ns = 1000000000.0 / 60.0;
-        long timer = System.currentTimeMillis();
-        int updates = 0;
-        int frames = 0;
         while (running) {
-            long now = System.nanoTime();
-            delta += (now - lastTime) / ns;
-            lastTime = now;
-            if (delta >= 1.0) {
-                update();
-                updates++;
-                delta--;
-            }
+            update();
             render();
-            frames++;
-            if (System.currentTimeMillis() - timer > 1000) {
-                timer += 1000;
-                System.out.println(updates + " ups, " + frames + " fps");
-                updates = 0;
-                frames = 0;
-            }
             if (glfwWindowShouldClose(window))
                 running = false;
         }
@@ -116,17 +74,13 @@ public class Main implements Runnable {
         glfwTerminate();
     }
 
+
     private void update() {
         glfwPollEvents();
-        level.update();
-        if (level.isGameOver()) {
-            level = new Level();
-        }
     }
 
     private void render() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        level.render();
 
         int error = glGetError();
         if (error != GL_NO_ERROR)
